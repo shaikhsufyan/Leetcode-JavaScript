890eee596b7f36fcd05a879a9a9792f055a478ea
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-05-11 11:02:00 +0530

    Find Smallest Number

--- FindSmallestNum/index.js
+++ FindSmallestNum/index.js
@@ -3,10 +3,13 @@ let num = [22,39,92,54,4,82,1,85];
 const findSmallest = (num) =>{
     let smallest = num[0];
     for(let i=0; i<num.length; i++){
-        if(num[i] < smallest){
-            smallest = num[i]
+        if(num[i] < smallest){  
+            smallest = num[i]  
         }
     }
     return smallest
 }
 console.log(findSmallest(num));
+
+
+ 
