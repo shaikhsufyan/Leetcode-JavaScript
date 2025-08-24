21b944e50190637ab6b21831b5b8a64a65ed9c6c
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-08-24 11:53:07 +0530

    Nullish coalescing operator in javascript

---
+++ Nullisn coalescing/index.js
@@ -0,0 +1,25 @@
+// WHAT IS NULLISH COALESCING OPERATOR
+
+// ?? is the nullish coalescing operator in
+//  JavaScript.
+
+// It returns the right-hand value only if  
+// the left-hand value is null or undefined.
+
+
+let name = null ?? "Default Value";
+console.log(name);
+
+
+ 
+
+
+
+
+// It’s different from || (OR), which also treats false, 0, "", and NaN as falsy.
+
+
+// "" false, NaN, 0
+// OR Example
+let x = "";
+console.log(x || "OR Default Value");
