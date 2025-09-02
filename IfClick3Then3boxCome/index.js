79955defe4c89df7170d5802a2da1b21e2c9c9b1
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-09-02 10:57:10 +0530

    Updated

--- IfClick3Then3boxCome/index.js
+++ IfClick3Then3boxCome/index.js
@@ -1,12 +1,21 @@
-let inputContainer = document.querySelector(".inputs");
-const handleInput = (event) =>{
-    console.log(event.target.value);
-    inputContainer.innerHTML=""
-    let value = event.target.value;
-    for(let i=0; i< value; i++){
-        let input = document.createElement("input");
-        inputContainer.appendChild(input);
-        input.style.width="200px"
-        input.style.marginTop="10px"
-    }
-}
+//  const inputContainer = document.querySelector(".inputs");
+
+//  const handleInput = (event) =>{
+//     console.log(event.target.value);
+//     let value = event.target.value; //1
+//     inputContainer.innerHTML = ""; // empty
+
+//     for(let i=0; i<value; i++){
+//         let input = document.createElement("input");
+//         inputContainer.appendChild(input);
+//         input.style.marginTop="10px"
+//     }
+    
+
+//  }
+
+const nums = [1,2,3,4,5];
+const res = nums.filter((curItem,i,arr)=>{
+    return curItem%2 ===0
+})
+console.log(res);
