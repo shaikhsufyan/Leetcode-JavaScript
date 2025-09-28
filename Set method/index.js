1d4fb276cd5801058cd2deffa6f31c811d89c6b7
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-09-28 12:17:53 +0530

    Set()  in javascript

---
+++ Set method/index.js
@@ -0,0 +1,19 @@
+// set() method in javascript
+// set method in which we can store unique
+// element of any data type primitive or object
+// automatically remove duplicate element
+// it return an object
+
+let set  = new Set(["CODE With","Sufyan"]);
+set.add("Shaikh");
+set.delete("Shaikh")
+set.clear()
+console.log(set);
+
+ 
+
+
+
+
+
+ 
