a437bd27db7a8539550cf9877fa4c37b47269698
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-10-02 11:07:14 +0530

    Web APIs in Javascript

---
+++ Browser Methods/index.js
@@ -0,0 +1,12 @@
+// This methods are part of Browser.
+// This methods are not part of javascript
+setInterval(()=>{})
+setTimeout(()=>{})
+// DOM API 
+console.log();
+
+
+
+// This all methods are called Web APIs.
+// This Web APIs are part of Browser
+// This Web APIs are not part of javascript
