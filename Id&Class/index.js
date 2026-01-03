b5132c6c7e5fa1098506b4e9027e0c64f8ca77f5
shaikh sufyan <shaikhsufyan1811@gmail.com>
2026-01-03 17:54:01 +0530

    Difference between ID and class

---
+++ Id&Class/index.js
@@ -0,0 +1,48 @@
+
+
+
+console.log(7 == 7);  
+
+console.log(7 == 7 == 7);
+
+console.log(7== 7 == 7 == 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    ID                   class
+//  Must be Unique        class can be reuse
+//  one element target    Multiple element target
+//  getElementById()      getElementByClassName()
+//  #Id                   .class
+//  High priority         Low priority
