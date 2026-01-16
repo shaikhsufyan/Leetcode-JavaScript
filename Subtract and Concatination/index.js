f0125a168789282430f86361466e86cd3480d134
shaikh sufyan <shaikhsufyan1811@gmail.com>
2026-01-16 10:36:34 +0530

    Subtract & Concatination

---
+++ Subtract and Concatination/index.js
@@ -0,0 +1,43 @@
+
+
+// "2" - 1     // 1
+// "2" + 1     // "21"   (because + triggers string concatenation)
+// "2" * 1     // 2
+// "2" - "1"   // 1
+// "2a" - 1    // NaN (because "2a" cannot be converted to a valid number)
+// "2"-1 = 1   because - operator tries to convert string number in numeric and it subtract perfect But in case "2"+1 here when we use + so it cocatinate because it string so it do concatinate
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sum = "2"+1;
+// let sub = "2"-1;
+
+// console.log(sum);
+// console.log(sub);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let res = "1"+2 - "1"-2;
+// // 12
+// console.log(res);
