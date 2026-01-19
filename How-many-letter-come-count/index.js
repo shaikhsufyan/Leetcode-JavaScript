2c2212d5523d3383b45873ca6c4b4b89d5f2982f
shaikh sufyan <shaikhsufyan1811@gmail.com>
2026-01-19 23:34:22 +0530

    Find character count

---
+++ How-many-letter-come-count/index.js
@@ -0,0 +1,42 @@
+// let name = "code with sufyan shaikh";
+
+// let obj={}
+// for(x of name){
+//     if(obj[x]){
+//         obj[x] = obj[x]+1
+//     }else{
+//         obj[x]=1
+//     }
+// }
+ 
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let arrA = [1,2];
+ let arrB = arrA.slice();
+ arrB[0]=42;
+ console.log(arrB);
+ 
+ 
+
+ 
+ 
+
+
+
+
