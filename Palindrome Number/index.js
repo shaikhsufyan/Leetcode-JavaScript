5ce0b656252a268012ec2ac5ac345add28112876
shaikh sufyan <shaikhsufyan1811@gmail.com>
2026-01-28 00:09:42 +0530

    Palindrome Number

---
+++ Palindrome Number/index.js
@@ -0,0 +1,41 @@
+
+// Check Palindrome Number
+
+// const isPalindromeNum = (num) =>{
+//     let original = num.toString();
+//     let reversed = original.split("").reverse().join("");
+
+//     if(original === reversed){
+//         console.log(num+" is Palindrome Number");
+        
+//     }else{
+//         console.log(num+" is not palindrome number");
+        
+//     }
+// }
+// isPalindromeNum(122)
+
+
+
+// let sentance = "hello madam how are you";
+// let arr = sentance.toLocaleLowerCase()
+// .split(" ");
+
+// arr.forEach((curWord)=>{
+//     let reversed = curWord.split("")
+//     .reverse().join("");
+
+//     if(reversed === curWord){
+//         console.log(curWord+ " is palindrome");
+        
+//     }else{
+//         console.log(curWord +" is not palindrome");
+        
+//     }
+// })
+
+
+
+
+
+ 
