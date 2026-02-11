88c20be1086488bd927755ef6a8103eb72c446ea
shaikh sufyan <shaikhsufyan1811@gmail.com>
2026-02-12 00:49:16 +0530

    Function-Hoisting in JS

---
+++ Fundtion-hoisting/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+function greets(){
+    console.log("greet 1");
+    
+}
+function greets(){
+    console.log("greet 2");
+    
+}
+ 
+ 
+greets()
+function greets(){
+    console.log("greet 1");
+    
+}
+greets()
+function greets(){
+    console.log("greet 2");
+     
+}
+greets()
